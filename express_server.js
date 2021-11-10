c7dd3ce756f3c447a8b2ed39c2a8b53b60dd95f8
stew anoya <stewartanoya@gmail.com>
2021-11-10 20:59:41 +0000

    added conditionals to handle non-signed-in users

--- express_server.js
+++ express_server.js
@@ -5,9 +5,20 @@ const cookieParser = require("cookie-parser");
 app.set("view engine", "ejs");
 
 //////URL DATABASE//////////////////
+// const urlDatabase = {
+//   b2xVn2: "http://www.lighthouselabs.ca",
+//   "9sm5xK": "http://www.google.com",
+// };
+
 const urlDatabase = {
-  b2xVn2: "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com",
+  b6UTxQ: {
+    longURL: "https://www.tsn.ca",
+    userID: "aJ48lW",
+  },
+  i3BoGr: {
+    longURL: "https://www.google.ca",
+    userID: "aJ48lW",
+  },
 };
 ////////////////////////////////////
 
@@ -34,8 +45,15 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+app.get("/", (req, res) => {
+  res.render("tinyappHome");
+});
+
 app.get("/urls", (req, res) => {
   const user = req.cookies["user_id"];
+  if (!user) {
+    res.redirect("/");
+  }
   const templateVars = { urls: urlDatabase, user: users[user] };
   res.render("urls_index", templateVars);
 });
@@ -45,6 +63,9 @@ app.get("/login", (req, res) => {
 });
 
 app.get("/urls/new", (req, res) => {
+  if (!req.cookies["user_id"]) {
+    res.status(403).redirect("/register");
+  }
   const user = req.cookies["user_id"];
   const templateVars = { user: users[user] };
   res.render("urls_new", templateVars);
@@ -52,17 +73,28 @@ app.get("/urls/new", (req, res) => {
 
 app.get("/urls/:shortURL", (req, res) => {
   const user = req.cookies["user_id"];
-  const templateVars = {
-    shortURL: req.params.shortURL,
-    longURL: urlDatabase[req.params.shortURL],
-    user: users[user],
-  };
-  res.render("urls_show", templateVars);
+  if (!user) {
+    res.redirect("/login");
+  } else if (urlDatabase[req.params.shortURL]["userID"] !== user) {
+    res.redirect("/login");
+  } else {
+    const templateVars = {
+      shortURL: req.params.shortURL,
+      longURL: urlDatabase[req.params.shortURL]["longURL"],
+      user: users[user],
+    };
+    res.render("urls_show", templateVars);
+  }
 });
 
 app.get("/u/:shortURL", (req, res) => {
-  const longURL = urlDatabase[req.params.shortURL];
-  res.redirect(302, longURL);
+  const user = req.cookies["user_id"];
+  const templateVars = {
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[req.params.shortURL]["longURL"],
+    user: users[user],
+  };
+  res.redirect(302, templateVars["longURL"]);
 });
 
 app.get("/register", (req, res) => {
@@ -73,34 +105,58 @@ app.get("/register", (req, res) => {
 
 app.post("/logout", (req, res) => {
   let user = req.cookies["user_id"];
+  //deletes cookie after signing out
   res.clearCookie("user_id", user);
   res.redirect(302, `/urls`);
 });
 
 app.post("/urls", (req, res) => {
   let shortURL = generateRandomString();
-  urlDatabase[shortURL] = req.body.longURL;
+  urlDatabase[shortURL] = {
+    longURL: req.body.longURL,
+    userID: req.cookies["user_id"],
+  };
   res.redirect(302, `/urls/${shortURL}`);
 });
 
 app.post("/urls/:shortURL/delete", (req, res) => {
-  delete urlDatabase[req.params.shortURL];
-  res.redirect(302, `/urls`);
+  let user = req.cookies["user_id"];
+  console.log("Here is my user", user);
+  console.log(
+    "here is what I want to compare it to",
+    urlDatabase[req.params.shortURL]["userID"]
+  );
+  //conditional check to see if person is signed in and to make sure id of url
+  //match id of user before deleting
+  if (!user) {
+    res.status(403);
+    res.redirect("/login");
+  } else if (urlDatabase[req.params.shortURL]["userID"] !== user) {
+    res.status(403);
+    res.redirect("/login");
+  } else {
+    delete urlDatabase[req.params.shortURL];
+    res.redirect(302, `/urls`);
+  }
 });
 
 app.post("/urls/:shortURL/edit", (req, res) => {
-  urlDatabase[req.params.shortURL] = req.body.longURL;
-  res.redirect(302, `/urls`);
+  // editing the longurl to whatever is put into form
+  urlDatabase[req.params.shortURL]["longURL"] = req.body["longURL"];
+
+  res.redirect(302, `/urls/${req.params.shortURL}`);
 });
 
 app.post("/login", (req, res) => {
   const emailCheck = emailLookup(req.body.email);
   const passCheck = passwordLookup(req.body.password);
-
+  // checks 'database' to see if emailcheck and passcheck are true
   if (emailCheck && passCheck) {
+    // if they are it stores the cookie with the userID
     res.cookie("user_id", emailCheck["id"]);
     res.redirect(302, "/urls");
   } else {
+    // it will throw an error and message otherwise
     res.status(403);
     res.send(
       "Your email or password was incorrect, please go back and try again."
@@ -110,9 +166,12 @@ app.post("/login", (req, res) => {
 
 app.post("/register", (req, res) => {
   let userid = generateRandomString();
+
+  // check to see if email is taken and throws error
   if (emailLookup(req.body.email)) {
     res.status(400);
     res.send("Email is taken, please go back and try again.");
+    // check to see if email or password are empty fields and throws error
   } else if (!req.body.email || !req.body.password) {
     res.status(400);
     res.send(

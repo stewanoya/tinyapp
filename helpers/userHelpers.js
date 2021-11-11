8969bdfe066f719a204a56751276200e03995014
stew anoya <stewartanoya@gmail.com>
2021-11-11 21:11:23 +0000

    Moved string generator to helper functions

--- helpers/userHelpers.js
+++ helpers/userHelpers.js
@@ -18,4 +18,67 @@ const passwordLookup = (password, db) => {
   }
   return false;
 };
-module.exports = { emailLookup, passwordLookup };
+
+const generateRandomString = () => {
+  let strRandom = "";
+  let alph = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+  ];
+  for (let i = 0; i < 7; i++) {
+    strRandom += alph.join("").charAt(Math.floor(Math.random() * alph.length));
+  }
+  return strRandom;
+};
+
+module.exports = { emailLookup, passwordLookup, generateRandomString };
